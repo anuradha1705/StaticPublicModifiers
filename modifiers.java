0bb45e37c762aecb1c65942c04c16e16bf67a4af
DevByAnu <anuradhakumawat2005@gmail.com>
2025-05-27 22:16:26 +0530

    Add files via upload

---
+++ modifiers.java
@@ -0,0 +1,19 @@
+public class modifiers {
+  // Static method
+  static void myStaticMethod() {
+    System.out.println("Static methods can be called without creating objects");
+  }
+
+  // Public method
+  public void myPublicMethod() {
+    System.out.println("Public methods must be called by creating objects");
+  }
+
+  // Main method
+  public static void main(String[] args) {
+    myStaticMethod(); 
+
+    modifiers myObj = new modifiers(); 
+    myObj.myPublicMethod(); 
+  }
+}
